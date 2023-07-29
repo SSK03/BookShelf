6a0523fafdd676370282a12249c52171c69ea683
SSK <seyitjankurbanov07@gmail.com>
2023-07-29 03:13:30 +0300

    Fourth Commit

--- app/src/main/java/com/example/bookshelf/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/bookshelf/ui/screens/HomeScreen.kt
@@ -1,2 +1,21 @@
 package com.example.bookshelf.ui.screens
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.bookshelf.ui.BooksUiState
+
+@Composable
+fun HomeScreen(
+    booksUiState: BooksUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    when (booksUiState) {
+        is BooksUiState.Loading -> LoadingScreen(modifier)
+        is BooksUiState.Succes -> BooksGridScreen(
+            books = booksUiState.bookSearch,
+            modifier = modifier
+        )
+        is BooksUiState.Error -> ErrorScreen(retryAction = retryAction, modifier)
+    }
+}
